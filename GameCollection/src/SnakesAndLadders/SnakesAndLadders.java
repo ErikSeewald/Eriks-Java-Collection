19b2559149d7a55e3f5add284f86d8b1df28e7e3
ErikSeewald <erik.seewaldx@gmail.com>
2022-11-05 14:20:29 +0100

    added movement and multiplayer to Snakes and Ladders

--- GameCollection/src/SnakesAndLadders/SnakesAndLadders.java
+++ GameCollection/src/SnakesAndLadders/SnakesAndLadders.java
@@ -1,11 +1,7 @@
 package SnakesAndLadders;
 
 import java.awt.BorderLayout;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.JFrame;
-
 import Main.MainMenu;
 import Main.WindowEventHandler;
 
@@ -20,6 +16,7 @@ public class SnakesAndLadders extends JFrame
 	{
 		panel = new SnL_Panel();
 		GUI = new SnL_GUI(panel);
+		panel.addGUI(GUI);
 		
 		this.add(panel, BorderLayout.WEST);
 		this.add(GUI, BorderLayout.EAST);
@@ -29,24 +26,6 @@ public class SnakesAndLadders extends JFrame
 		this.setTitle("Snakes and Ladders");
 		this.setResizable(false);
 		this.setVisible(true);
-		
-		this.addKeyListener(new KeyListener() 
-		{
-			@Override
-			public void keyPressed(KeyEvent e) 
-			{
-				int code = e.getKeyCode();
-				//45 -- Minus
-				//521 -- Plus
-				
-				if (code == 45) {panel.changePlayerPos(-1);} //-
-				if (code == 521) {panel.changePlayerPos(1);} //+
-			}
-			@Override
-			public void keyReleased(KeyEvent e) {}
-			@Override
-			public void keyTyped(KeyEvent e) {}
-		});
 	}
 	
 	public void start(WindowEventHandler eventHandler)
