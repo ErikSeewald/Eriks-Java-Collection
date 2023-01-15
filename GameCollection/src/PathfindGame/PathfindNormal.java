f42dbabb2efa98e17fa791d8e04c6f3f0dda34c5
Erik Seewald <erik.seewaldx@gmail.com>
2023-01-15 13:10:14 +0100

    completely overhauled Pathfind gameplay

--- GameCollection/src/PathfindGame/PathfindNormal.java
+++ GameCollection/src/PathfindGame/PathfindNormal.java
@@ -7,8 +7,6 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -24,16 +22,31 @@ public class PathfindNormal extends JPanel implements ActionListener
 	private int BOX_SIZE = (PANEL_SIZE /GRID_SIZE)+1;
 	
 	//BOARD
-	private int[][] board = new int[GRID_SIZE][GRID_SIZE];
+	private class Square
+	{
+		int hp = 3;
+		boolean isAlive;
+		
+		Square(boolean isAlive)
+		{this.isAlive = isAlive;}
+		
+		public void getHit()
+		{
+			hp--;
+			if (hp <= 0)
+			{isAlive = false;}
+		}
+	}
+	private Square[][] board;
 	
 	//CHASER
 	private class Chaser
 	{
 		int x,y;
-		int RM_count = 0; //how many times has the chaser needed to make a random move because something is blocking the direct path
+		boolean prefersX;
 		
-		Chaser(int x, int y)
-		{this.x = x; this.y = y;}
+		Chaser(int x, int y, boolean prefersX)
+		{this.x = x; this.y = y; this.prefersX = prefersX;}
 	}
 	private int chaser_count = 10;
 	private Chaser[] chasers = new Chaser[chaser_count];
@@ -46,63 +59,48 @@ public class PathfindNormal extends JPanel implements ActionListener
 	}
 	Player player = new Player();
 	
-	Timer timer;
+	//OTHERS
+	private Timer timer;
+	private Random random = new Random();
 	
-	Random random = new Random();
-	
-	Color halfred = new Color(200,50,50);
-	
-	boolean moveDone = true; 	//is the chaser move done? -> Player move ready
-	
-	boolean finished = false;	//have the chasers caught the player?
-	
-	//numbers used to push the counter coordinates on the two axis depending on the digit count
-	int counterpush1;
-	double counterpush2;
-	
+	private boolean moveDone = true; 	//is the chaser move done? -> Player move ready
+	private boolean finished = false;
+	private int seed = random.nextInt();
 	
 	PathfindNormal()
 	{	
 		this.setPreferredSize(new Dimension (PANEL_SIZE,PANEL_SIZE));
-		
-		ClickListener clickListener = new ClickListener();
-		this.addMouseListener(clickListener);
-		
 		timer = new Timer(100, this);
 		
-		start(true);
+		reset();
 	}
 	
-	public int[][] getBoard()
-	{return board;}
+	public void setSeed(int seed)
+	{this.seed = seed;}
 	
-	public int[][] getChasers()
+	public void restart()
+	{initialize(false);}
+	
+	public void reset()
+	{initialize(true);}
+	
+	private void initialize(boolean fullReset)
 	{
-		int[][] ch = new int[2][chaser_count];
-		for (int i = 0; i < chaser_count; i++)
-		{
-			ch[0][i] = chasers[i].x;
-			ch[1][i] = chasers[i].y;
-		}
-		return ch;
-	}
-	
-	public int[] getSizes()
-	{int[] sizes = {chaser_count, GRID_SIZE}; return sizes;}
-	
-	public int[] getPlayer()
-	{int[] playerArr = {player.x, player.y, player.move_count}; return playerArr;}
-	
-	public void start(boolean fullReset)
-	{
+		random.setSeed(seed);
+		seed+=10;
+		
 		if (fullReset)
 		{
-			for (int i = 0; i < GRID_SIZE; i++)
+			board = null;
+			System.gc();
+			board = new Square[GRID_SIZE][GRID_SIZE];
+			
+			for (int x = 0; x < GRID_SIZE; x++)
 			{
-				for (int j = 0; j < GRID_SIZE; j++)
-				{
-					if (random.nextInt(4) == 1) {board[i][j] = 1;}
-					else {board[i][j] = 0;}	
+				for (int y = 0; y < GRID_SIZE; y++)
+				{	
+					boolean isAlive = random.nextInt(4) == 1;
+					board[x][y] = new Square(isAlive);
 				}
 			}
 		}
@@ -113,31 +111,57 @@ public class PathfindNormal extends JPanel implements ActionListener
 		chasers = new Chaser[chaser_count];
 		for (int i = 0; i < chaser_count; i++)
 		{
-			chasers[i] = new Chaser(random.nextInt(GRID_SIZE),random.nextInt(GRID_SIZE));
+			boolean validPos = false;
+			while(!validPos)
+			{
+				int x = random.nextInt(GRID_SIZE);
+				int y = random.nextInt(GRID_SIZE);
+				
+				validPos = !board[x][y].isAlive;
+				
+				if (validPos)
+				{chasers[i] = new Chaser(x,y, random.nextBoolean());}
+			}
 		}
 		
-		finished = false;
 		
 		//PLAYER
-		player.x = random.nextInt(GRID_SIZE);
-		player.y = random.nextInt(GRID_SIZE);
+		boolean validPos = false;
+		while(!validPos)
+		{
+			player.x = random.nextInt(GRID_SIZE);
+			player.y = random.nextInt(GRID_SIZE);	
+			
+			if (board[player.x][player.y].isAlive) {continue;} //generate new coordinates
+			
+			validPos = true;
+			for (Chaser chaser : chasers)
+			{
+				if (player.x == chaser.x && player.y == chaser.y)
+				{validPos = false; break;}
+			}
+		}
+			
 		player.move_count = 0;
 		
+		finished = false;
 		repaint();
 	}
 	
-	public void load(int[]sizes, int[][]loadboard, int[][]loadchaser, int[] loadplayer)
+	public void load(int[]sizes, boolean[][]loadboard, int[][]loadchaser, int[] loadplayer)
 	{
 		GRID_SIZE = sizes[1];
+		BOX_SIZE = (PANEL_SIZE /GRID_SIZE)+1;
 		chaser_count = sizes[0];
 		
-		for (int i = 0; i < GRID_SIZE; i++)
+		//BOARD
+		for (int x = 0; x < GRID_SIZE; x++)
+		{
+			for (int y = 0; y < GRID_SIZE; y++)
 			{
-				for (int j = 0; j < GRID_SIZE; j++)
-				{
-					board[i][j] = loadboard[i][j];
-				}
+				board[x][y].isAlive = loadboard[x][y];
 			}
+		}
 		
 		//CHASERS
 		chasers = null;
@@ -145,44 +169,16 @@ public class PathfindNormal extends JPanel implements ActionListener
 		chasers = new Chaser[chaser_count];
 		for (int i = 0; i < chaser_count; i++)
 		{
-			chasers[i] = new Chaser(loadchaser[0][i], loadchaser[1][i]);
+			chasers[i] = new Chaser(loadchaser[0][i], loadchaser[1][i], random.nextBoolean());
 		}
 		
-		finished = false;
-		
 		//PLAYER
-		player.x = loadplayer[0];;
+		player.x = loadplayer[0];
 		player.y = loadplayer[1];
 		player.move_count = loadplayer[2];
 		
-		BOX_SIZE = (PANEL_SIZE /GRID_SIZE)+1;
+		finished = false;
 		repaint();
-	}
-	
-	private class ClickListener extends MouseAdapter
-	{
-	    public void mousePressed(MouseEvent e) 
-	    {  
-	    	if (!finished)
-	    	{
-	    		int column = e.getX() / BOX_SIZE;
-	    		int row = e.getY() / BOX_SIZE;
-	    	
-	    		if (e.isShiftDown())
-	    		{
-	    			if (board[row][column] == 0) {board[row][column] = 1;}
-	    			else {board[row][column] = 0;}
-	    		}
-	    	
-	    		else
-	    		{
-	    			player.x = column;
-	    			player.y = row;
-	    		}
-	    	
-	    		repaint();
-	    	}
-	    }
 	}
 		
 	@Override
@@ -197,65 +193,65 @@ public class PathfindNormal extends JPanel implements ActionListener
 		for (Chaser c : chasers)
 		{nextStep(c);}
 		
-		repaint();
-		
 		moveDone = true;
+		repaint();
 	}
 	
 	public void nextStep(Chaser chaser)
 	{	
-		
-		if (chaser.RM_count == 0)
+		//MOVE
+		if (chaser.prefersX)
 		{
-			if (random.nextBoolean())
-			{	
-				if (xMove(chaser)) {return;}	
-				if (yMove(chaser)) {return;}	
-			}
+			if (xMove(chaser)) {return;}	
+			if (yMove(chaser)) {return;}
+		}
 			
-			else 
-			{
-				if (yMove(chaser)) {return;}	
-				if (xMove(chaser)) {return;}	
-			}
-		}
-		
-		chaser.RM_count++;
-		
-		if (chaser.RM_count < 3)
+		else
 		{
-			switch (random.nextInt(4))
-			{
-				case 0: if (isValidMove(chaser.x+1, chaser.y))  {chaser.x++; break;}
-				case 1: if (isValidMove(chaser.x-1, chaser.y)) {chaser.x--; break;}
-				case 2: if (isValidMove(chaser.x, chaser.y+1))  {chaser.y++; break;}
-				case 3: if (isValidMove(chaser.x, chaser.y-1)) {chaser.y--; break;}
-			}
+			if (yMove(chaser)) {return;}	
+			if (xMove(chaser)) {return;}
 		}
-	
-		else {chaser.RM_count = 0;}
-	}
-	
-	public boolean isValidMove(int column, int row)
-	{
-		if (column > -1 && column < GRID_SIZE &&  row > -1 && row < GRID_SIZE)
-		{return (board[row][column] == 0);}
-		return false;
+		
+		//NO VALID MOVE FOUND -> START ATTACKING SQUARE
+		int attackX = chaser.x;
+		int attackY = chaser.y;
+		
+		if (chaser.prefersX)
+		{
+			if (player.x > chaser.x)
+			{attackX++;}
+				
+			else if (player.x < chaser.x)
+			{attackX--;}
+			
+		}
+		
+		else 
+		{
+			if (player.y > chaser.y)
+			{attackY++;}
+				
+			if (player.y < chaser.y)
+			{attackY--;}
+		}
+		
+		board[attackX][attackY].getHit();
 	}
 	
 	public boolean xMove(Chaser chaser)
 	{
 		if (player.x > chaser.x)
 		{
-			if (board[chaser.y] [chaser.x + 1] == 0) 
+			if (!board[chaser.x + 1] [chaser.y].isAlive) 
 			{chaser.x++; return true;}
 		}
 		
 		else if (player.x < chaser.x)
 		{
-			if (board[chaser.y] [chaser.x - 1] == 0) 
+			if (!board[chaser.x - 1] [chaser.y].isAlive) 
 			{chaser.x--; return true;}
 		}
+		
 		return false;
 	}
 	
@@ -263,31 +259,16 @@ public class PathfindNormal extends JPanel implements ActionListener
 	{		
 		if (player.y > chaser.y)
 		{
-			if (board[chaser.y + 1] [chaser.x] == 0) 
+			if (!board[chaser.x] [chaser.y + 1].isAlive) 
 			{chaser.y++; return true;}
 		}
 		
 		else if (player.y < chaser.y)
 		{
-			if (board[chaser.y - 1] [chaser.x] == 0) 
+			if (!board[chaser.x] [chaser.y - 1].isAlive) 
 			{chaser.y--; return true;}
 		}
 		return false;
-	}
-	
-	
-	public void wipeBoard()
-	{
-		if (!finished)
-		{
-			for (int i = 0; i < GRID_SIZE; i++)
-			{
-				for (int j = 0; j < GRID_SIZE; j++)
-				{board[i][j] = 0;}
-			}
-		
-			repaint();
-		}
 	}
 	
 	public void movePlayer(char key)
@@ -315,6 +296,13 @@ public class PathfindNormal extends JPanel implements ActionListener
 		}
 	}
 	
+	public boolean isValidMove(int x, int y)
+	{
+		if (x > -1 && x < GRID_SIZE &&  y > -1 && y < GRID_SIZE)
+		{return (!board[x][y].isAlive);}
+		return false;
+	}
+	
 	public void paint(Graphics g)
 	{
 		for (Chaser chaser : chasers)
@@ -326,23 +314,27 @@ public class PathfindNormal extends JPanel implements ActionListener
 		Graphics2D g2D = (Graphics2D) g;
 		super.paint(g2D);
 		
-		g2D.setPaint(Color.DARK_GRAY);
 		
-		for (int row = 0; row < GRID_SIZE; row++)
+		//BOARD
+		for (int x = 0; x < GRID_SIZE; x++)
 		{
-			for (int column = 0; column < GRID_SIZE; column++)
+			for (int y = 0; y < GRID_SIZE; y++)
 			{	
-				if (board[row][column] != 0) 
-				{g2D.fillRect(column * BOX_SIZE, row * BOX_SIZE, BOX_SIZE, BOX_SIZE);}
+				g2D.setPaint(Color.DARK_GRAY);
+				if (board[x][y].hp == 2) {g2D.setPaint(Color.GRAY);}
+				else if (board[x][y].hp == 1) {g2D.setPaint(Color.LIGHT_GRAY);}
 				
-				g2D.drawRect(column * BOX_SIZE, row * BOX_SIZE, BOX_SIZE, BOX_SIZE); //Grid	
+				if (board[x][y].isAlive) 
+				{g2D.fillRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);}
+				
+				g2D.drawRect(x * BOX_SIZE, y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
 			}
 		}
 		
 		g2D.setPaint(new Color (0, 140, 250));
 		g2D.fillRect(player.x * BOX_SIZE, player.y * BOX_SIZE, BOX_SIZE, BOX_SIZE);
 		
-		if (!finished) {g2D.setPaint(halfred.brighter());}
+		if (!finished) {g2D.setPaint(new Color(200,50,50).brighter());}
 		else {g2D.setPaint(new Color (50, 250, 50));}
 		
 		for (Chaser chaser : chasers)
@@ -352,24 +344,27 @@ public class PathfindNormal extends JPanel implements ActionListener
 		
 		if (!finished)
 		{
+			int counterpush = 65;
+			
 			g2D.setPaint(Color.DARK_GRAY);
 			g2D.setFont(new Font (null, Font.PLAIN, PANEL_SIZE/40));
-			if (player.move_count >= 100) {counterpush1 = 350;}
-			else if (player.move_count >= 10) {counterpush1 = 110;}
-			else {counterpush1 = 65;}
+			if (player.move_count >= 100) {counterpush = 350;}
+			else if (player.move_count >= 10) {counterpush = 110;}
+			else {counterpush = 65;}
 			g2D.drawString
-			(""+player.move_count, player.x * BOX_SIZE + (PANEL_SIZE/counterpush1), player.y * BOX_SIZE + (PANEL_SIZE/30));
+			(""+player.move_count, player.x * BOX_SIZE + (PANEL_SIZE/counterpush), player.y * BOX_SIZE + (PANEL_SIZE/30));
 		}
 		
 		else 
 		{
-			g2D.setPaint(halfred);
+			double counterpush = 2.7;
+			
+			g2D.setPaint(new Color(200,50,50));
 			g2D.setFont(new Font (null, Font.PLAIN, PANEL_SIZE/2));
-			if (player.move_count >= 100) {counterpush2 = 11.6;}
-			else if (player.move_count >= 10) {counterpush2 = 4.7;}
-			else {counterpush2 = 2.7;}
+			if (player.move_count >= 100) {counterpush = 11.6;}
+			else if (player.move_count >= 10) {counterpush = 4.7;}
 			g2D.drawString
-			(""+player.move_count, (int)(GRID_SIZE/counterpush2 * BOX_SIZE), (int)(GRID_SIZE/1.5 * BOX_SIZE));
+			(""+player.move_count, (int)(GRID_SIZE/counterpush * BOX_SIZE), (int)(GRID_SIZE/1.5 * BOX_SIZE));
 		}
 		
 		
@@ -381,4 +376,34 @@ public class PathfindNormal extends JPanel implements ActionListener
 		repaint(); 
 		this.setPreferredSize(new Dimension(PANEL_SIZE,PANEL_SIZE));
 	}
+	
+	public boolean[][] getBoard()
+	{
+		boolean[][] b = new boolean[GRID_SIZE][GRID_SIZE];
+		for (int x = 0; x < GRID_SIZE; x++)
+		{
+			for (int y = 0; y < GRID_SIZE; y++)
+			{
+				b[x][y] = board[x][y].isAlive;
+			}
+		}
+		return b;
+	}
+	
+	public int[][] getChasers()
+	{
+		int[][] ch = new int[2][chaser_count];
+		for (int i = 0; i < chaser_count; i++)
+		{
+			ch[0][i] = chasers[i].x;
+			ch[1][i] = chasers[i].y;
+		}
+		return ch;
+	}
+	
+	public int[] getSizes()
+	{int[] sizes = {chaser_count, GRID_SIZE}; return sizes;}
+	
+	public int[] getPlayer()
+	{int[] playerArr = {player.x, player.y, player.move_count}; return playerArr;}
 }
