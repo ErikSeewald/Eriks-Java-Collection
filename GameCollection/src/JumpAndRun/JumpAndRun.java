451887749959547c08c171ca8311e587a3935289
ErikSeewald <erik.seewaldx@gmail.com>
2023-01-18 14:58:58 +0100

    began refactoring JnR

--- GameCollection/src/JumpAndRun/JumpAndRun.java
+++ GameCollection/src/JumpAndRun/JumpAndRun.java
@@ -3,7 +3,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.ArrayList;
+import java.util.HashSet;
+
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
@@ -12,20 +13,20 @@ import Main.WindowEventHandler;
 
 public class JumpAndRun 
 {
-	int fallingSpeed = 5;
-	int timeUntilFall = 0;
+	private int timeUntilFall = 0;
+	private int scrollingSpeed;
+	private int xSpeed, ySpeed;
 	
-	static Timer timer;			//decides the framerate (at 14 -> around 60fps)
-
-	int scrollingSpeed;
+	private static Timer timer;			//decides the framerate (at 14 -> around 60fps)
 	
 	public void start(WindowEventHandler eventHandler, boolean fastMode) 
 	{
-		
+		//INITIALIZATION
 		JFrame frame = new JFrame("Sidescroller");
 		if (fastMode) {frame.setTitle("Speedrun");}
 		
 		frame.setIconImage(MainMenu.img.getImage());
+		frame.addWindowListener(eventHandler);
 		
 		if (fastMode) 
 		{scrollingSpeed = 0;}
@@ -34,54 +35,47 @@ public class JumpAndRun
 		
 		JumpAndRunPanel panel = new JumpAndRunPanel(scrollingSpeed);
 		
-		frame.addWindowListener(eventHandler);
-		
 		frame.add(panel);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setVisible(true);
 		
-		ArrayList<Integer> pressedKeys = new ArrayList<>();
+		//MOVEMENT
+		HashSet<Integer> pressedKeys = new HashSet<>();
 		
 		timer = new Timer(14, new ActionListener() 
 		{	@Override
 			public void actionPerformed(ActionEvent e) 
 			{
-				int x = 0;
-				int y = 0;
-				
+				xSpeed = 0;
+			
 				if (pressedKeys.contains(68)) //D
-				{if (fastMode) {x = 10;} else {x = 5;}} 	
+				{xSpeed = 5;} 	
 				
 				if (pressedKeys.contains(65)) //A
-				{if (fastMode) {x = -10;} else {x = -5;}}
+				{xSpeed = -5;}
 				
-				if (panel.flyMode)
+				//JUMPING
+				if (pressedKeys.contains(32) && panel.player.jumpAllowed) //SPACE
 				{
-					if (pressedKeys.contains(87)) {y = -5;} 	//W
-					if (pressedKeys.contains(83)) {y = 5;} 		//S
-					fallingSpeed = 0;
+					ySpeed = -5;
+					panel.player.jumpAllowed = false; 
+					timeUntilFall = 35;
 				}
+					
+				//FALLING
+				if (timeUntilFall > 0) {timeUntilFall--;}
+					
+				if (timeUntilFall == 10) {ySpeed = 1;}	//for a smoother transition in fallingSpeed
+				else if (timeUntilFall == 0) {ySpeed = 5;}
+					
+				if (panel.player.onGround) {panel.player.jumpAllowed = true;}
 				
-				else 
-				{
-					if (pressedKeys.contains(32) && panel.jumpAllowed) //SPACE
-					{
-						fallingSpeed = -5; panel.jumpAllowed = false; 
-						timeUntilFall = 35;	//countdown until player begins falling again starts
-					}
-					 
-					
-					if (timeUntilFall > 0) {timeUntilFall--;}	//counting down
-					
-					if (timeUntilFall == 10) {fallingSpeed = 1;}	//for a smoother transition in fallingSpeed
-					else if (timeUntilFall == 0) {fallingSpeed = 5;}
-					
-					if (panel.onGround) {panel.jumpAllowed = true;}
-				}
 				
 				panel.offset+=panel.scrollingSpeed;
-				panel.movePlayer(x,y,fallingSpeed);	
+				
+				if (fastMode) {xSpeed*=2;}
+				panel.movePlayer(xSpeed,ySpeed);	
 			}	
 		});
 		
@@ -98,19 +92,12 @@ public class JumpAndRun
 			{	
 				int code = e.getKeyCode();
 				
-//				if (code ==521) {if (panel.changeSize(10)) {frame.pack();}}		//+
-//				else if (code ==45) {if (panel.changeSize(-10)) {frame.pack();}}	//-
-				
-				if (!(pressedKeys.contains(code))) {pressedKeys.add(code);}
-				
-				if (code == 70) {panel.flyMode = !panel.flyMode;}	//F
-				
-				else if (code == 82) {panel.start(false,false);} //R Restart
+				if (code == 82) {panel.start(false,false);} //R Restart
 				else if (code == 84) {panel.start(true,false);} //T	Restart + new map
 				
 				//CTRL S
-				else if (code == 83 && e.isControlDown()) 
-				{if (!panel.paused) {pause();} panel.saveMap(); pressedKeys.remove(pressedKeys.indexOf(83));} //otherwise S stays pressed
+				else if (code == 83 && e.isControlDown())
+				{if (!panel.paused) {pause();} panel.saveMap(); pressedKeys.remove(83);} //otherwise S stays pressed
 				
 				//CTRL L
 				else if (code == 76 && e.isControlDown()) {if (!panel.paused) {pause();} panel.loadMap(); frame.pack();} 
@@ -118,14 +105,16 @@ public class JumpAndRun
 				else if (code == 27) //ESC
 				{pause();} 
 				
+				else
+				{pressedKeys.add(code);}
+				
 			}
 			@Override
 			public void keyReleased(KeyEvent e) 
 			{
-				int x = pressedKeys.indexOf(e.getKeyCode());
-				pressedKeys.remove(x);
+				pressedKeys.remove(e.getKeyCode());
 			}	
-			
+	
 			public void pause()
 			{panel.paused = !panel.paused; panel.repaint(); if (timer.isRunning()) {timer.stop();} else {timer.start();}}
 		});	
