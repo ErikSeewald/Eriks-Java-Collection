ef4895f7b85b6eb8a1bb88f039af71e536209648
ErikSeewald <erik.seewaldx@gmail.com>
2023-02-13 08:00:03 +0100

    created separate file for ReflectionAlgorithm

---
+++ GameCollection/src/reflectionDemo/Ray.java
@@ -0,0 +1,36 @@
+package reflectionDemo;
+
+public class Ray 
+{
+	private static final double PI = Math.PI;
+	
+	double[] origin = new double[2];
+	private double angle;
+	
+	Ray(double x, double y, double angle)
+	{
+		this.origin[0] = x; this.origin[1] = y;
+		this.angle = angle;
+	}
+	
+	public double getAngle()
+	{return angle;}
+	
+	public void setAngle(double angle)
+	{
+		this.angle = angle;
+		this.toValidAngle();
+	}
+	
+	public void rotate(double angle)
+	{
+		this.angle+=angle;
+		this.toValidAngle();
+	}
+	
+	private void toValidAngle()
+	{
+		if (this.angle < 0) {this.angle+=2*PI;}
+		if (this.angle > 2*PI) {this.angle -=2*PI;}
+	}
+}
