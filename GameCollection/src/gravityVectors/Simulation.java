fcb4a90cc7bcdac2b24eb7bf699a6ffebb82fef0
ErikSeewald <erik.seewaldx@gmail.com>
2023-02-21 12:39:00 +0100

    created separate file for gravity vecs simulation

--- GameCollection/src/gravityVectors/Simulation.java
+++ GameCollection/src/gravityVectors/Simulation.java
@@ -5,102 +5,92 @@ import Main.EJC_Util;
 
 public class Simulation 
 {
-	//ARROW VARIABLES
-	final static int APOINT_SIZE = 6;
-	final static int ARROW_LENGTH = 12;
-	final static int APOINT_DISTANCE = APOINT_SIZE * 5; //distance between arrows
-	int APOINT_COUNT_X = 30; //how many arrows on x axis
-	int APOINT_COUNT_Y = APOINT_COUNT_X * 2/3; //how many arrows on y axis
+	//ARROWS
+	private static final int ARROW_DISTANCE = 30; //distance between arrows
+	private int arrow_countX = 30;
+	private int arrow_countY = arrow_countX * 2/3;
 	ArrowPoint[] arrowPoints;
 	
-	//PULL/GRAVITY POINT VARIABLES
+	//PULLPOINTS
 	PullPoint selectedPoint;
 	ArrayList<PullPoint> pullPoints;
 	
-	boolean validDrag = false;
-	private boolean normalizeVector = true;
+	private boolean validDrag = false;
+	private boolean normalize = true;
 	
 	Simulation()
 	{pullPoints = new ArrayList<>(); pullPoints.add(new PullPoint());}
 	
 	public class ArrowPoint
 	{
-		ArrowPoint(int locX, int locY)
-		{this.locX = locX; this.locY = locY; arrowX = locX + 15;  arrowY = locY;}
+		static final int length = 12;
 		
 		//LOCATIONS OF THE ACTUAL POINTS
-		int locX;
-		int locY;
-		
+		int locX, locY;
+
 		//LOCATIONS OF THE ENDS OF THE ARROWS
-		int arrowX;
-		int arrowY;
+		int arrowX, arrowY;
+		
+		ArrowPoint(int locX, int locY)
+		{this.locX = locX; this.locY = locY; arrowX = locX + 15;  arrowY = locY;}
 	}
 	
 	public class PullPoint
 	{
 		public static final int size = 12;
-		
-		int locX = 30;
-		int locY = 30;
+		int locX = 30, locY = 30;
 	}
 	
 	private void simulate()
 	{
-		int ppoint_count = pullPoints.size();
+		int point_count = pullPoints.size();
 		
 		for (ArrowPoint arrow : arrowPoints)
 		{
-			float[] vectorsX = new float[ppoint_count];
-			float[] vectorsY = new float[ppoint_count];
-			
-			for (int i = 0; i < ppoint_count; i++)
+			float[] vectorsX = new float[point_count], vectorsY = new float[point_count];
+			for (int i = 0; i < point_count; i++)
 			{
 				vectorsX[i] = pullPoints.get(i).locX - arrow.locX;
 				vectorsY[i] = pullPoints.get(i).locY - arrow.locY;
 			}
 			
-			float[] distances = new float[ppoint_count];
-			
-			for (int i = 0; i < ppoint_count; i++)
+			float[] distances = new float[point_count];
+			for (int i = 0; i < point_count; i++)
 			{
 				distances[i] = (float) Math.sqrt(Math.pow((vectorsX[i]),2) + Math.pow((vectorsY[i]),2));
 			}
 			
-			//HAVE THE PPOINTS EFFECT THE ARROWS EXPONENTIALLY MORE, THE SMALLER THE DISTANCE BETWEEN THEM IS (0.99^distance)
+			//HAVE THE POINTS EFFECT THE ARROWS EXPONENTIALLY MORE, THE SMALLER THE DISTANCE BETWEEN THEM IS (0.99^distance)
 			//-> HIGHER DISTANCE = SMALLER NUMBER WITH WHICH THE VECTOR WILL END UP BEING MULTIPLIED
-			float[] strengths = new float[ppoint_count];
-			for (int i = 0; i < ppoint_count; i++)
+			float[] strengths = new float[point_count];
+			for (int i = 0; i < point_count; i++)
 			{strengths[i] = (float) Math.pow(0.99, distances[i]);}
 			
 			//NORMALIZE VECTORS SO "STRENGTHS" CAN ACTUALLY AFFECT THE VECTORS PROPORTIONALLY
-			for (int i = 0; i < ppoint_count; i++)
+			for (int i = 0; i < point_count; i++)
 			{
-				float[] temp = EJC_Util.normalize(vectorsX[i], vectorsY[i]);
-				vectorsX[i] = temp[0];
-				vectorsY[i] = temp[1];
+				vectorsX[i]/= distances[i];
+				vectorsY[i]/= distances[i];
 			}
 			
-			float arrowVectX = 0;
-			float arrowVectY = 0;
+			float arrowVectX = 0, arrowVectY = 0;
 			
 			//COMBINE ALL THE VECTORS MULTIPLIED BY THEIR STRENGTH
-			for (int i = 0; i < ppoint_count; i++)
+			for (int i = 0; i < point_count; i++)
 			{
 				arrowVectX+= vectorsX[i] * strengths[i];
 				arrowVectY+= vectorsY[i] * strengths[i];
 			}
 			
-			//NORMALIZE ONE FINAL TIME TO MULTIPLY WITH ARROW_LENGTH
-			if (normalizeVector)
+			if (normalize)
 			{
 				float[] temp = EJC_Util.normalize(arrowVectX, arrowVectY);
 				arrowVectX = temp[0];
 				arrowVectY = temp[1];
 			}
 			
-			arrow.arrowX = (int) (arrow.locX + arrowVectX * ARROW_LENGTH);
-			arrow.arrowY = (int) (arrow.locY + arrowVectY * ARROW_LENGTH);
+			arrow.arrowX = (int) (arrow.locX + arrowVectX * ArrowPoint.length);
+			arrow.arrowY = (int) (arrow.locY + arrowVectY * ArrowPoint.length);
 		}
 	}
 	
@@ -108,18 +98,15 @@ public class Simulation
 	
 	public void initialize()
 	{
-		//CLEAR OLD ARRAY FROM MEMORY, SET UP NEW ONE WITH ADJUSTED SIZE
-		arrowPoints = null;
-		System.gc();
-		arrowPoints = new ArrowPoint[APOINT_COUNT_X * APOINT_COUNT_Y];
-
-		//ARROWPOINTS
+		arrowPoints = null; System.gc();
+		arrowPoints = new ArrowPoint[arrow_countX * arrow_countY];
+		
 		int index = 0;
-		for (int i = 0; i < APOINT_COUNT_X; i++)
+		for (int i = 0; i < arrow_countX; i++)
 		{
-			for (int j = 0; j < APOINT_COUNT_Y; j++)
+			for (int j = 0; j < arrow_countY; j++)
 			{
-				arrowPoints[index] = new ArrowPoint(i*APOINT_DISTANCE+(APOINT_DISTANCE / 3), j*APOINT_DISTANCE+(APOINT_DISTANCE / 3));
+				arrowPoints[index] = new ArrowPoint(i*ARROW_DISTANCE+(ARROW_DISTANCE / 3), j*ARROW_DISTANCE+(ARROW_DISTANCE / 3));
 				index++;
 			}
 		}
@@ -129,11 +116,11 @@ public class Simulation
 	
 	public void switchArrowNormalization()
 	{
-		normalizeVector = !normalizeVector;
+		normalize = !normalize;
 		simulate();
 	}
 	
-	public void selectGravityPoint(int X, int Y)
+	public void selectPoint(int x, int y)
 	{	
 		validDrag = false;
 		
@@ -141,25 +128,24 @@ public class Simulation
 		{
 			if 
 			(
-				X > point.locX-10 && X < point.locX+10 &&
-				Y > point.locY-10 && Y < point.locY+10 
+				x > point.locX-10 && x < point.locX+10 &&
+				y > point.locY-10 && y < point.locY+10 
 			)
-			{selectedPoint = point; validDrag = true; break;}
+			{selectedPoint = point; validDrag = true; return;}
 		}
 	}
 	
-	public void moveGravityPoint(int X, int Y)
+	public void movePoint(int x, int y)
 	{
-		if (validDrag)
-		{
-			selectedPoint.locX = X;
-			selectedPoint.locY = Y;
-		}
+		if (!validDrag)
+		{return;}
 		
+		selectedPoint.locX = x;
+		selectedPoint.locY = y;
 		simulate();
 	}
 	
-	public void changePPOINT_COUNT(int amount)
+	public void changePointCount(int amount)
 	{	
 		if (pullPoints.size() + amount < 1) {return;}
 
@@ -173,7 +159,8 @@ public class Simulation
 	
 	public void increaseArrowCount()
 	{
-		APOINT_COUNT_X++;
-		APOINT_COUNT_Y = APOINT_COUNT_X * 2/3;
+		arrow_countX++;
+		arrow_countY = arrow_countX * 2/3;
+		initialize();
 	}
 }
