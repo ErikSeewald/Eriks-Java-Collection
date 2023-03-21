4a127a367c4305a8c8284d5e1fc50d7c20e151e4
Erik Seewald <erik.seewaldx@gmail.com>
2023-03-21 10:56:09 +0100

    started working on Bomb Sorting

--- GameCollection/src/Main/MainMenu.java
+++ GameCollection/src/Main/MainMenu.java
@@ -3,11 +3,13 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import bloonShoot.EJC_BloonShoot;
+import bomb_sorting.EJC_Bomb_Sorting;
 import clothSim.EJC_ClothSim;
 import gravityVectors.EJC_GravityVectors;
 import insects.EJC_Insects;
@@ -77,7 +79,7 @@ public class MainMenu extends JFrame implements MouseListener
 				new GButton("      Pathfind", 4), new GButton("    Parallel U.", 5), new GButton("    Reflection", 6), new GButton(" Mouse Dodge", 7), 
 				new GButton("   Sidescroller", 8), new GButton(" Pixel Collision", 9), new GButton("    Cloth Sim", 10), new GButton(" Gravity Vects", 11),
 				new GButton("  Bloon Shoot", 12), new GButton("  Rand Battle", 13), new GButton(" Rand Growth", 14), new GButton("     Ladders", 15), 
-				new GButton("", 16), new GButton("", 17), new GButton("", 18), new GButton("", 19), 
+				new GButton(" Bomb Sorting", 16), new GButton("", 17), new GButton("", 18), new GButton("", 19), 
 				new GButton("", 20), new GButton("", 21), new GButton("", 22), new GButton("", 23), 
 		};
 		
@@ -158,6 +160,7 @@ public class MainMenu extends JFrame implements MouseListener
 			case 13: new EJC_RandBattle().start(eventHandler); break;
 			case 14: new EJC_RandGrowth().start(eventHandler); break;
 			case 15: new EJC_SnakesAndLadders().start(eventHandler); break;
+			case 16: new EJC_Bomb_Sorting().start(eventHandler); break;
 			
 			default: return;
 		}
---
+++ GameCollection/src/bomb_sorting/Sort_Panel.java
@@ -0,0 +1,73 @@
+package bomb_sorting;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class Sort_Panel extends JPanel
+{
+	private static final long serialVersionUID = -7304963195064748685L;
+	private int PANEL_HEIGHT = 700;
+	private int PANEL_WIDTH = (int) (PANEL_HEIGHT * 1.5);
+	
+	// PLATES
+	private int plate_width;
+	private int plate_height;
+	private int plate_x_left;
+	private int plate_x_right;
+	private int plate_y;
+	
+	Sort_Panel()
+	{
+		this.setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));
+		this.init_sizes();
+	}
+	
+	public void init_sizes()
+	{
+		plate_width = (int) (PANEL_WIDTH / 3.5);
+		plate_height = PANEL_HEIGHT / 2;
+		plate_x_left = PANEL_HEIGHT / 60;
+		plate_x_right = PANEL_WIDTH - plate_width - plate_x_left;
+		plate_y = (int) (PANEL_HEIGHT / 2.6);
+	}
+	
+	public void stop()
+	{}
+	
+	//---------------------------------------PAINT---------------------------------------
+	
+	private static final Color background_col = new Color(50, 50, 55);
+	private static final Color border_col = new Color(80, 80, 85);
+	private static final Color left_plate_col = new Color(205, 30, 30);
+	private static final Color right_plate_col = new Color(30, 30, 30);
+	
+	public void paint(Graphics g)
+	{
+		Graphics2D g2D = (Graphics2D) g;
+		
+		//BACKGROUND
+		g2D.setPaint(background_col);
+		g2D.fillRect(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+		
+		
+		//PLATES
+		g2D.setPaint(left_plate_col);
+		g2D.fillRect(plate_x_left, plate_y, plate_width, plate_height);
+		
+
+		g2D.setPaint(right_plate_col);
+		g2D.fillRect(plate_x_right, plate_y, plate_width, plate_height);
+		
+		//PLATE BORDERS
+		g2D.setStroke(new BasicStroke(PANEL_HEIGHT / 60));
+		g2D.setPaint(border_col);
+		g2D.drawRect(plate_x_left, plate_y, plate_width, plate_height);
+		g2D.drawRect(plate_x_right, plate_y, plate_width, plate_height);
+		
+	}
+}
