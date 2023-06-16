76f4861c2ee4730538aeb595ff3dca9d523b6393
Erik Seewald <erik.seewaldx@gmail.com>
2023-06-16 12:39:11 +0200

    more stubs

---
+++ GameCollection/src/taxCollector/MapHandler.java
@@ -0,0 +1,11 @@
+package taxCollector;
+
+public class MapHandler 
+{
+	private int[][] map;
+	
+	MapHandler()
+	{
+		map = new int[1000][1000];
+	}
+}
--- GameCollection/src/taxCollector/TC_Panel.java
+++ GameCollection/src/taxCollector/TC_Panel.java
@@ -1,9 +1,33 @@
 package taxCollector;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 public class TC_Panel extends JPanel
 {
 	private static final long serialVersionUID = -5017286155233171011L;
-
+	
+	private int PANEL_HEIGHT = 700;
+	private int PANEL_WIDTH = (int) (PANEL_HEIGHT * 1.52);
+	
+	TC_Panel()
+	{
+		this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+	}
+	
+	//---------------------------------------PAINT---------------------------------------
+	
+	private static final Color background_col = new Color(55, 55, 70);
+	
+	public void paint(Graphics g)
+	{
+		Graphics2D g2D = (Graphics2D) g;
+		
+		//BACKGROUND
+		g2D.setPaint(background_col);
+		g2D.fillRect(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+	}
 }
