ea5c9e839b8f6ae16b3d6bb8d9538575c7506446
ErikSeewald <erik.seewaldx@gmail.com>
2023-06-23 14:23:48 +0200

    added trees

---
+++ GameCollection/src/taxCollector/Tree.java
@@ -0,0 +1,26 @@
+package taxCollector;
+
+import java.util.Random;
+
+public class Tree extends MapItem
+{
+	public static final int size_tiles = 2;
+	public static final float trunk_width_tiles = 0.5f;
+	public static final float trunk_height_tiles = 1.5f;
+	
+	public static enum ColorStates
+	{
+		NORMAL, BRIGHTER, DARKER;
+	};
+	
+	ColorStates color_state;
+
+	Tree(int i, int j, Random random)
+	{
+		super(i, j);
+		color_state = ColorStates.values()[random.nextInt(3)];
+	}
+	
+	@Override
+	public int getTileSize() {return 0;}
+}
