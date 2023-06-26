0634ab3a9f7f88dffcc6fbf4dc88152cc8c7c0f0
ErikSeewald <erik.seewaldx@gmail.com>
2023-06-26 16:33:52 +0200

    updated jar

--- GameCollection/src/taxCollector/mapItem/House.java
+++ GameCollection/src/taxCollector/mapItem/House.java
@@ -21,7 +21,7 @@ public class House extends MapItem
 	
 	private void initValues(Random random)
 	{
-		tax = random.nextDouble(300) + 10;
+		tax = random.nextDouble(200) + 10;
 		cooldown = random.nextInt(90000) + 10000;
 	}
 	
